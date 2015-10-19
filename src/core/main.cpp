1c5653e42bf372952b9f73fb576bae81c4b2f6e2
Daniel Bengtsson <daniel-bengtsson@live.com>
2015-10-19 17:34:53 +0200

    Updated main with console menu

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,22 +1,48 @@
 #include "Serializer.h"
+#include <algorithm>
 #include <iostream>
+#include <string>
+
+void PrintHelp();
+void ReadMesh();
 
 int main()
 {
-	Serializer importer;
-	if (importer.OpenFileForRead("totem.mesh")) 
+	PrintHelp();
+
+	bool quit = false;
+	while ( !quit )
 	{
-		int* n = new int(0);
-		n = (int*)(importer.ReadBytesFromFile(sizeof(int)));
-		float* pos = (float*)(importer.ReadBytesFromFile(*n));
-		n = (int*)(importer.ReadBytesFromFile(sizeof(int)));
-		float* norm = (float*)(importer.ReadBytesFromFile(*n));
-		n = (int*)(importer.ReadBytesFromFile(sizeof(int)));
-		float* tan = (float*)(importer.ReadBytesFromFile(*n));
-		n = (int*)(importer.ReadBytesFromFile(sizeof(int)));
-		float* bitan = (float*)(importer.ReadBytesFromFile(*n));
-		n = (int*)(importer.ReadBytesFromFile(sizeof(int)));
-		float* tex = (float*)(importer.ReadBytesFromFile(*n));
+		std::cout << "> ";
+		std::string input;
+		std::cin >> input;
+		std::transform( input.begin(), input.end(), input.begin(), ::tolower );
+
+		if ( input == "quit" || input == "exit" || input == "q" )
+			quit = true;
+
+		if ( input == "mesh" || input == "m" )
+			ReadMesh();
+	}
+	return 0;
+}
+
+void ReadMesh()
+{
+	Serializer importer;
+	if ( importer.OpenFileForRead( "totem.mesh" ) )
+	{
+		int* n = new int( 0 );
+		n = ( int* )( importer.ReadBytesFromFile( sizeof( int ) ) );
+		float* pos = ( float* )( importer.ReadBytesFromFile( *n ) );
+		n = ( int* )( importer.ReadBytesFromFile( sizeof( int ) ) );
+		float* norm = ( float* )( importer.ReadBytesFromFile( *n ) );
+		n = ( int* )( importer.ReadBytesFromFile( sizeof( int ) ) );
+		float* tan = ( float* )( importer.ReadBytesFromFile( *n ) );
+		n = ( int* )( importer.ReadBytesFromFile( sizeof( int ) ) );
+		float* bitan = ( float* )( importer.ReadBytesFromFile( *n ) );
+		n = ( int* )( importer.ReadBytesFromFile( sizeof( int ) ) );
+		float* tex = ( float* )( importer.ReadBytesFromFile( *n ) );
 
 		delete n;
 		delete pos;
@@ -27,12 +53,20 @@ int main()
 
 		importer.CloseFile();
 	}
-	if (importer.OpenFileForRead("totem.mesh"))
+	if ( importer.OpenFileForRead( "totem.mesh" ) )
 	{
-		float* data = (float*)(importer.ReadBytesFromFile());
+		float* data = ( float* )( importer.ReadBytesFromFile() );
 		delete data;
 
 		importer.CloseFile();
 	}
-	return 0;
+}
+
+void PrintHelp()
+{
+	std::cout << "The following commands are supported\n\n";
+
+	std::cout << "- Read .mesh (mesh)\n";
+
+	std::cout << "\n";
 }
