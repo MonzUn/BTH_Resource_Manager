7591aef2d6f83bd86760777454e179a3ef231b81
Daniel Bengtsson <daniel-bengtsson@live.com>
2015-10-21 23:49:22 +0200

    PacaWriter now excludes .paca-files

--- src/core/PacaWriter.cpp
+++ src/core/PacaWriter.cpp
@@ -9,6 +9,7 @@ void PacaWriter::WritePaca( const std::string& startingDirectory )
 	std::vector<std::string> filePaths;
 	std::vector<std::string> subfolderPaths;
 
+	// Recursively look through all subfolders for resources
 	subfolderPaths.push_back( startingDirectory );
 	do
 	{
@@ -18,19 +19,38 @@ void PacaWriter::WritePaca( const std::string& startingDirectory )
 		std::vector<std::string> contentPaths;
 		FileUtility::GetListOfContentInDirectory( directoryToSearch, contentPaths );
 		for ( int i = 0; i < contentPaths.size(); ++i )
-			contentPaths[i].find( '.' ) == std::string::npos ? subfolderPaths.push_back( directoryToSearch + contentPaths[i] ) : filePaths.push_back( directoryToSearch + contentPaths[i] ); // Sort the content into subfolders and files
-
+		{
+			if ( contentPaths[i].find( '.' ) == std::string::npos ) // It's a subfolder
+			{
+				subfolderPaths.push_back( directoryToSearch + contentPaths[i] );
+			}
+			else // It's a resource
+			{
+				if ( contentPaths[i].find( ".paca" ) == std::string::npos ) // Don't store .paca in .paca
+					filePaths.push_back( directoryToSearch + contentPaths[i] );
+			}
+		}
 		
 	} while ( !subfolderPaths.empty() );
 
 	unsigned int dataTotalByteSize = 0;
 
+	// Find all fileLengths
 	std::vector<unsigned int> fileLengths;
 	for ( int i = 0; i < filePaths.size(); ++i )
 	{
 		unsigned int fileByteSize = FileUtility::GetFileContentSize( filePaths[i], std::ios::binary );
 		fileLengths.push_back( fileByteSize );
-		dataTotalByteSize += dataTotalByteSize;
+		dataTotalByteSize += fileByteSize;
+	}
+
+	// Load all file content
+	std::vector<char*> fileContents;
+	for ( int i = 0; i < filePaths.size(); ++i )
+	{
+		char* fileContent = static_cast<char*>( malloc( fileLengths[i] ) );
+		FileUtility::GetFileContent( filePaths[i], fileContent, std::ios::binary );
+		fileContents.push_back( fileContent );
 	}
 
 	unsigned int headerByteSize = filePaths.size() * sizeof( unsigned int ) * 2; // One hash and one length variable for each entry
@@ -61,6 +81,13 @@ void PacaWriter::WritePaca( const std::string& startingDirectory )
 		walker += sizeof( unsigned int );
 	}
 
+	// Write all the data
+	for ( int i = 0; i < fileContents.size(); ++i )
+	{
+		memcpy( walker, fileContents[i], fileLengths[i] );
+		walker += fileLengths[i];
+	}
+
 	std::ofstream outStream; 
 	outStream.open( startingDirectory + "/assets.paca", std::ios::binary );
 	if ( outStream.is_open() )
@@ -71,4 +98,6 @@ void PacaWriter::WritePaca( const std::string& startingDirectory )
 	assert( walker - totalByteSize == buffer );
 
 	free( buffer );
+	for ( int i = 0; i < fileContents.size(); ++i )
+		free( fileContents[i] );
 }
