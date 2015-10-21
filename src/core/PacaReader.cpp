08c6d1d087c72ddb45b97cf37cad17543d6f2189
Daniel Bengtsson <daniel-bengtsson@live.com>
2015-10-22 00:31:23 +0200

    PAcareader can now fetch resources and their respective size

--- src/core/PacaReader.cpp
+++ src/core/PacaReader.cpp
@@ -30,7 +30,7 @@ bool PacaReader::Open( const std::string & filePath )
 				unsigned int pathHash, resourceByteSize;
 				pathHash			= *reinterpret_cast<unsigned int*>( headerData + ( sizeof( unsigned int ) * i ) );
 				resourceByteSize	= *reinterpret_cast<unsigned int*>( headerData + ( objectCount * sizeof( unsigned int ) ) + sizeof( unsigned int ) * i );
-				m_HeaderInfo.emplace( pathHash, ResoureMetaData( nextStartIndex, resourceByteSize ) );
+				m_HeaderInfo.emplace( pathHash, ResoureMetaData( sizeof( unsigned int ) +  headerSize + nextStartIndex, resourceByteSize ) );
 				nextStartIndex += resourceByteSize;
 			}
 
@@ -59,4 +59,40 @@ bool PacaReader::IsOpen() const
 std::string PacaReader::GetOpenPath() const
 {
 	return m_OpenFilePath;
+}
+
+unsigned int PacaReader::GetResourceSize( const std::string& resourcePath ) const
+{
+	unsigned int resourceSize = 0;
+	unsigned int pathHash = HashString( resourcePath );
+	if ( m_HeaderInfo.find( pathHash ) != m_HeaderInfo.end() )
+	{
+		resourceSize = m_HeaderInfo.at( pathHash ).ByteSize;
+	}
+
+	return resourceSize;
+}
+
+bool PacaReader::GetResource( const std::string& resourcePath, void* outBuffer, unsigned int outBufferSize )
+{
+	bool result = false;
+	unsigned int pathHash = HashString( resourcePath );
+	if ( m_HeaderInfo.find( pathHash ) != m_HeaderInfo.end() )
+	{
+		const ResoureMetaData& resourceMetaData = m_HeaderInfo.at( pathHash );
+		if ( resourceMetaData.ByteSize <= outBufferSize )
+		{
+			m_InStream.seekg( resourceMetaData.StartIndex );
+			m_InStream.read( static_cast<char*>( outBuffer ), resourceMetaData.ByteSize );
+			result = true;
+		}
+	}
+
+	return result;
+}
+
+unsigned int PacaReader::HashString( const std::string& toHash ) const
+{
+	std::hash<std::string> hashFunction;
+	return hashFunction( toHash );
 }
--- src/core/PacaReader.h
+++ src/core/PacaReader.h
@@ -14,12 +14,17 @@ struct ResoureMetaData
 class PacaReader
 {
 public:
-	bool		Open( const std::string& filePath );
-	void		Close();
-	bool		IsOpen() const;
-	std::string GetOpenPath() const;
+	bool			Open( const std::string& filePath );
+	void			Close();
+	bool			IsOpen() const;
+	std::string		GetOpenPath() const;
+
+	unsigned int	GetResourceSize( const std::string& resourcePath ) const;
+	bool			GetResource( const std::string& resourcePath, void* outBuffer, unsigned int outBufferSize );
 
 private:
+	unsigned int HashString( const std::string& toHash ) const;
+
 	std::string									m_OpenFilePath;
 	std::ifstream								m_InStream;
 	std::map<unsigned int, ResoureMetaData>		m_HeaderInfo;
